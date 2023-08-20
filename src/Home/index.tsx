280f65163c68038838deaca528497d24b01a4970
unknown <lanhabuu@gmail.com>
2023-08-20 20:10:04 +0700

    first commit

---
+++ src/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+const Home = () => {
+	return (
+		<div>
+			<h2>Home</h2>
+			<Link to='https://shop.tiktok.com/view/product/1729649197670238624?region=VN&local=en' target='_blank'>
+				web
+			</Link>
+		</div>
+	)
+}
+
+export default Home
